1283fa34eae46a609badb77c073c892741e84a22
Galaxar <matteo.secco97@gmail.com>
2020-02-27 16:25:08 +0100

    Update pom.xml, update ReplicaList

--- src/main/java/dataStructures/ReplicaList.java
+++ src/main/java/dataStructures/ReplicaList.java
@@ -1,22 +1,27 @@
 package dataStructures;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-import java.util.List;
+import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class ReplicaList {
 
     private static ReplicaList instance;
+    @Getter
+    private Map<InetAddress,Integer> vectorClocks = new HashMap<>();
 
     public static ReplicaList getInstance(){
         if(instance==null) instance = new ReplicaList();
         return instance;
     }
 
-    public List<String> getAllReplicas(){
-        //TODO
-        return null;
-    };
+    public Set<InetAddress> getAllReplicas(){
+        return vectorClocks.keySet();
+    }
 }
