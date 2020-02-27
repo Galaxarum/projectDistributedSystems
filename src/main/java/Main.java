4473bb3a07fb01daed6d948170b79d6fb8df6211
Galaxar <matteo.secco97@gmail.com>
2020-02-27 17:37:25 +0100

    Update Main, ReplicaList (rationalized)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,38 @@
 import interfaces.DataStoreInterface;
 import interfaces.GroupCommunicationInterface;
+import network.ServerSocketListener;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.logging.Logger;
 
 public class Main {
+
+    public static final int DEFAULT_PORT = 12345;
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-        String knownPeer = args[0];
-        GroupCommunicationInterface groupCom = null; //=...
-        groupCom.join(knownPeer);
-        //Get command from client
-        DataStoreInterface<Integer,Object> dataStore = null;
-        dataStore.put(1,"ciao");
-        dataStore.get(1);
-        dataStore.delete(1);
-        groupCom.leave();
+        final int port = initPort(args);
+
+        try{
+            new Thread(new ServerSocketListener(new ServerSocket(port))).start();
+        } catch (IOException e) {
+            logger.severe("Unable to open ServerSocket");
+            e.printStackTrace();
+            return;
+        }
+
+        /*TODO: contact another replica
+          TODO: get aligned with the contacted replica
+         */
+
+    }
+
+    private static int initPort(String[] args){
+            try{
+                return Integer.parseInt(args[1]);
+            }catch (NumberFormatException | ArrayIndexOutOfBoundsException e){
+                return DEFAULT_PORT;
+            }
     }
 }
--- src/main/java/dataStructures/ReplicaList.java
+++ src/main/java/dataStructures/ReplicaList.java
@@ -4,7 +4,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.net.InetAddress;
+import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -14,14 +14,19 @@ public class ReplicaList {
 
     private static ReplicaList instance;
     @Getter
-    private Map<InetAddress,Integer> vectorClocks = new HashMap<>();
+    private Map<Socket,Integer> vectorClocks = new HashMap<>();
 
     public static ReplicaList getInstance(){
         if(instance==null) instance = new ReplicaList();
         return instance;
     }
 
-    public Set<InetAddress> getAllReplicas(){
+    public Set<Socket> getAllReplicas(){
         return vectorClocks.keySet();
     }
+
+    public void put(Socket socket){
+        vectorClocks.put(socket,0);
+    }
+
 }
