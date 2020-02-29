7b6e4cc0548871c9508ef8a73694bba4051272a6
Galaxar <matteo.secco97@gmail.com>
2020-02-29 19:21:50 +0100

    Update MessagingMiddleware Added generic types

--- middleware/src/main/java/middleware/MessagingMiddleware.java
+++ middleware/src/main/java/middleware/MessagingMiddleware.java
@@ -1,10 +1,12 @@
 package middleware;
 
+import middleware.primitives.DataOperations;
+
 import java.io.IOException;
 import java.util.List;
 
-public interface MessagingMiddleware {
-    void join(String knownHost) throws IOException, ClassNotFoundException;
+public interface MessagingMiddleware <K,V>{
+    void join(String knownHost) throws IOException;
     void leave();
-    void sendVectorClockedMessage(VectorClockedMessage message);
+    void shareOperation(DataOperations command, K key, V value);
 }
