a0daf9d524b5d460fd7dc546f170b7c8a45e44ca
Galaxar <matteo.secco97@gmail.com>
2020-02-29 19:28:12 +0100

    Update Main Rationalized execution flow

--- application/src/main/java/distributedStorage/Main.java
+++ application/src/main/java/distributedStorage/Main.java
@@ -1,8 +1,12 @@
 package distributedStorage;
 
+import distributedStorage.database.DatabaseManager;
+import distributedStorage.network.ClientConnectionManager;
+import lombok.Getter;
 import middleware.MessagingMiddleware;
 import middleware.MessagingMiddlewareImpl;
 
+import java.io.IOException;
 import java.util.logging.Logger;
 
 public class Main {
@@ -12,24 +16,68 @@ public class Main {
     public static final int KNOWN_HOST_INDEX = 1;
     public static final int MIDDLEWARE_PORT_INDEX = 2;
     public static final int CLIENT_PORT_INDEX = 3;
+    public static final int ILLEGAL_PORT = -1;
+    public static final String FIRST_REPLICA_DISCRIMINATOR = "first";
+    @Getter
+    private static MessagingMiddleware<String,Object> messagingMiddleware;
+    @Getter
+    private static DatabaseManager databaseManager = new DatabaseManager();
 
     public static void main(String[] args) {
-        final String id;
-        final String knownHost;
-        MessagingMiddleware messagingMiddleware;
-        try {
-            id = args[ID_INDEX];
-            knownHost = args[KNOWN_HOST_INDEX];
-        }catch (ArrayIndexOutOfBoundsException e){
-            logger.severe("Id or known host not specified");
+        if(args.length<=Integer.max(ID_INDEX,KNOWN_HOST_INDEX)){
+            final String error = "Id or known host missing";
+            logger.severe(error);
+            throw new IllegalArgumentException(error);
+        }
+
+        final String id = args[ID_INDEX];
+        final String knownHost = args[KNOWN_HOST_INDEX];
+        final int middlewarePort = parsePortArgs(args,MIDDLEWARE_PORT_INDEX);
+        final int clientPort = parsePortArgs(args,CLIENT_PORT_INDEX);
+        messagingMiddleware = messagingMiddleware(id, middlewarePort);
+
+        new Thread(clientConnectionManager(clientPort)).start();
+
+        //Very first replica has no group to join
+        if(!FIRST_REPLICA_DISCRIMINATOR.equals(knownHost)) try {
+            messagingMiddleware.join(knownHost);
+        } catch (IOException e) {
+            logger.severe("Unable to join the group");
             return;
         }
-        try {
-            final int port = Integer.parseInt(args[MIDDLEWARE_PORT_INDEX]);
-            messagingMiddleware = new MessagingMiddlewareImpl(id,port);
-        } catch (ArrayIndexOutOfBoundsException | NumberFormatException e){
-            messagingMiddleware = new MessagingMiddlewareImpl(id);
-        }
-        messagingMiddleware.join(knownHost);
+
+        //Add a shutdownHook to leave before shutting down
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.finest("Leaving the group");
+            messagingMiddleware.leave();
+            logger.finest("Shutdown completed");
+        }));
     }
+
+    private static int parsePortArgs(String[] args, int index){
+        try{
+            return Integer.parseInt(args[index]);
+        }catch (ArrayIndexOutOfBoundsException | NumberFormatException e){
+            return ILLEGAL_PORT;
+        }
+    }
+
+    private static MessagingMiddleware<String,Object> messagingMiddleware(String id, int port){
+        return port==ILLEGAL_PORT?
+                new MessagingMiddlewareImpl(id):
+                new MessagingMiddlewareImpl(id,port);
+    }
+
+    private static ClientConnectionManager clientConnectionManager(int port) {
+        try {
+            return port == ILLEGAL_PORT ?
+                    new ClientConnectionManager() :
+                    new ClientConnectionManager(port);
+        }catch (IOException e){
+            logger.severe("cannot start ClientConnectionManager");
+            System.exit(0);
+            return null;
+        }
+    }
+
 }
