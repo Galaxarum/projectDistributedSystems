148b1361a0fde8027a8361c71f6e089aea0265e8
Alex Stabile <alex.stabile@lastminute.com>
2020-02-29 13:52:21 +0100

    Added JoinGroupUseCase and DatabaseManager

---
+++ middleware/src/main/java/middleware/VectorClocksManager.java
@@ -0,0 +1,35 @@
+package middleware;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class VectorClocksManager {
+
+    private static VectorClocksManager instance;
+
+    public static VectorClocksManager getInstance() {
+        if(instance==null) instance = new VectorClocksManager();
+        return instance;
+    }
+
+    /**
+     * Maps ids to clocks
+     */
+    private VectorClocks clocks = new VectorClocks();
+    
+    public VectorClocks getClocks(){
+   	 return clocks;
+    }
+    
+    public void add(String deviceId, Integer timestamp) {
+   	 clocks.put(deviceId, timestamp);
+    }
+    
+    public void remove(String deviceId) {
+   	 clocks.remove(deviceId);
+    }
+}
