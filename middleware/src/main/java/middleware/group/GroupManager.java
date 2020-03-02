11defd95c7fcf3fecb60f4141b290f689f10cab5
Galaxar <matteo.secco97@gmail.com>
2020-03-02 18:26:55 +0100

    Update GroupManager -Generified GroupManager -Made GroupManager.join return the initial data (received after synch) -Finalized JOIN and JOINING behaviour

--- middleware/src/main/java/middleware/group/GroupManager.java
+++ middleware/src/main/java/middleware/group/GroupManager.java
@@ -1,5 +1,6 @@
 package middleware.group;
 
+import exceptions.BrokenProtocolException;
 import lombok.SneakyThrows;
 import middleware.networkThreads.P2PConnection;
 import middleware.primitives.GroupCommands;
@@ -12,21 +13,23 @@ import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 
-public class GroupManager {
+import static middleware.primitives.GroupCommands.*;
 
-    private final String MY_DEVICE_NAME;
+public class GroupManager <K,V>{
+
+    private final String MY_ID;
     private final int PORT;
     private Map<String,Socket> socketMap = new HashMap<>();   //TODO: this must be shared with messaging middleware impl
     private Map<String,NodeInfo> replicas = new HashMap<>();
     private Socket targetSocket;
 
     public GroupManager(String id, int port) throws IOException {
-        this.MY_DEVICE_NAME = id;
+        this.MY_ID = id;
         this.PORT = port;
         new Thread(new ServerSocketRunnable<P2PConnection>(this.PORT)).start();
     }
 
-    public void join(String knownHost) throws IOException {
+    public Map<K,V> join(String knownHost) throws IOException {
         getReplicas(knownHost);
 
         new JoinGroupUseCase(this.targetSocket, this.replicas, this.MY_DEVICE_NAME, this.PORT).execute();
@@ -40,15 +43,26 @@ public class GroupManager {
         ObjectOutputStream out = new ObjectOutputStream(targetSocket.getOutputStream());
         ObjectInputStream in = new ObjectInputStream(targetSocket.getInputStream());
 
-        out.writeObject(GroupCommands.JOIN);
-        out.writeObject(MY_DEVICE_NAME);
+        out.writeObject(JOIN);
+        out.writeObject(MY_ID);
 
-        replicas = (Map<String, NodeInfo>) in.readObject();
+        try {
+            replicas = (Map<String, NodeInfo>) in.readObject();
+        }catch (ClassCastException e){
+            throw new BrokenProtocolException("Expected instance of "+ Map.class.getName()+" of parameters "+String.class.getName()+","+NodeInfo.class.getName(),e);
+        }
 
         for (Map.Entry<String,NodeInfo> singleNode :replicas.entrySet()) {
             final String id = singleNode.getKey();
             final NodeInfo nodeInfo = singleNode.getValue();
             Socket newSocket = new Socket(nodeInfo.getHostname(),nodeInfo.getPort());
+            try(ObjectOutputStream newOut = new ObjectOutputStream(newSocket.getOutputStream());
+                ObjectInputStream newIn = new ObjectInputStream(newSocket.getInputStream())){
+                    newOut.writeObject(JOINING);
+                    final GroupCommands ack = (GroupCommands) newIn.readObject();
+                    if(!ACK.equals(ack))
+                        throw new BrokenProtocolException("Expected "+ACK+" but "+newIn+" was received. This breaks the group joining protocol");
+            }
             socketMap.put(id, newSocket);
         }
     }
