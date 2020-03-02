84eb94143c69321d3c4c3aa21fa0108c23769aca
Galaxar <matteo.secco97@gmail.com>
2020-03-02 18:24:56 +0100

    Update PrimitiveParser, ServerSocketRunnable Finalized generic instantiation in ServerSocketRunnable Removed method PrimitiveParser.getInstance

--- middleware/src/main/java/templates/PrimitiveParser.java
+++ middleware/src/main/java/templates/PrimitiveParser.java
@@ -1,10 +1,12 @@
 package templates;
 
 import exceptions.ParsingException;
+import lombok.SneakyThrows;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.lang.reflect.ParameterizedType;
 import java.net.Socket;
 import java.util.logging.Logger;
 
@@ -89,7 +91,4 @@ public abstract class PrimitiveParser<T> implements Runnable{
      */
     protected abstract void parseCommand(T command) throws ParsingException;
 
-    public static PrimitiveParser getInstance(Socket clientSocket) throws IOException {
-        return null;
-    }
 }
--- middleware/src/main/java/templates/ServerSocketRunnable.java
+++ middleware/src/main/java/templates/ServerSocketRunnable.java
@@ -3,11 +3,13 @@ package templates;
 import middleware.networkThreads.P2PConnection;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.ParameterizedType;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Logger;
 
-public class ServerSocketRunnable<T extends PrimitiveParser> implements Runnable{
+public class ServerSocketRunnable<T extends PrimitiveParser> implements Runnable {
     /**
      * Used to accept connections with the other replicas
      */
@@ -19,6 +21,7 @@ public class ServerSocketRunnable<T extends PrimitiveParser> implements Runnable
 
     /**
      * Creates a ConnectionAcceptor listening to the given port
+     *
      * @param port The port to listen to
      * @throws IOException If thrown by {@linkplain ServerSocket#ServerSocket(int)}
      */
@@ -31,14 +34,28 @@ public class ServerSocketRunnable<T extends PrimitiveParser> implements Runnable
      */
     @Override
     public void run() {
-        while (!serverSocket.isClosed()){
+        while (!serverSocket.isClosed()) {
             try {
                 Socket clientSocket = serverSocket.accept();
-                new Thread(T.getInstance(clientSocket)).start();    //TODO: this calls PrimitiveParser.getInstance->Use a factory
+                new Thread(getInstanceOfT(clientSocket)).start();
             } catch (IOException e) {
-                logger.warning("An error occurred while accepting a connection by "+serverSocket);
+                logger.warning("An error occurred while accepting a connection by " + serverSocket);
             }
         }
 
     }
+
+    private T getInstanceOfT(Socket socket) throws IOException {
+        try {
+            ParameterizedType superClass = (ParameterizedType) getClass().getGenericSuperclass();
+            Class<T> type = (Class<T>) superClass.getActualTypeArguments()[0];
+            return type.getConstructor(Socket.class).newInstance(socket);
+        } catch (Exception e) {
+            String errorMsg = "Unable to reflectively construct an instance of generic parameter T. Printing exception stack trace";
+            logger.severe(errorMsg);
+            e.printStackTrace();
+            throw new IOException(errorMsg,e);
+        }
+    }
 }
+
