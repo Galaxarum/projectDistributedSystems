50c48bf9e15eec56c9ce9c4400fe4324c072a45e
Galaxar <matteo.secco97@gmail.com>
2020-03-03 12:14:29 +0100

    Moved DataOperations to application level

---
+++ application/src/main/java/primitives/DataOperations.java
@@ -0,0 +1,12 @@
+package primitives;
+
+import middleware.primitives.Primitive;
+
+/**
+ * This enum contains the applicative primitives
+ */
+public enum DataOperations implements Primitive {
+    PUT,
+    GET,
+    DELETE
+}
