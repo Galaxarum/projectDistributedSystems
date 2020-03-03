ebfd25047f14cd12959be6798cfc7c02d8c285b3
Galaxar <matteo.secco97@gmail.com>
2020-03-03 12:06:03 +0100

    Update MessagingMiddleware, MessagingMiddlewareImpl -Added generic type ApplicationPrimitive to both to allow to specify it at application level -Generified MessagingMiddlewareImpl

--- middleware/src/main/java/middleware/MessagingMiddlewareImpl.java
+++ middleware/src/main/java/middleware/MessagingMiddlewareImpl.java
@@ -1,29 +1,29 @@
 package middleware;
 
 import middleware.group.GroupManager;
-import middleware.primitives.DataOperations;
+import middleware.primitives.Primitive;
 
 import java.io.IOException;
 import java.util.Map;
 
-public class MessagingMiddlewareImpl implements MessagingMiddleware<String,Object> {
+public class MessagingMiddlewareImpl<Key,Value,ApplicationPrimitive extends Enum<ApplicationPrimitive> & Primitive> implements MessagingMiddleware<Key,Value,ApplicationPrimitive> {
 
     public static final int DEFAULT_PORT = 12345;
 
-    private final GroupManager<String,Object> groupManager;
+    private final GroupManager<Key,Value> groupManager;
 
 
-    public MessagingMiddlewareImpl(String id, int port) throws IOException {
-        this.groupManager = new GroupManager<>(id, port);
+    public MessagingMiddlewareImpl(String id, int port,String leaderHost) {
+        this.groupManager = new GroupManager<>(id, port,leaderHost);
     }
 
-    public MessagingMiddlewareImpl(String id) throws IOException {
-        this(id,DEFAULT_PORT);
+    public MessagingMiddlewareImpl(String id,String leaderHost) {
+        this(id,DEFAULT_PORT,leaderHost);
     }
 
     @Override
-    public Map<String, Object> join(String knownHost) throws IOException {
-        return groupManager.join(knownHost);
+    public Map<Key, Value> join() {
+        return groupManager.join();
     }
 
     @Override
@@ -32,7 +32,9 @@ public class MessagingMiddlewareImpl implements MessagingMiddleware<String,Objec
     }
 
     @Override
-    public void shareOperation(DataOperations command, String key, Object value) {
+    public void shareOperation(ApplicationPrimitive command, Key key, Value value) {
 
     }
+
+
 }
