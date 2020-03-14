e38a3729d909981c1f0214efbe015a66c67cc273
Giulia Lorini <giulia_lorini@fastwebnet.it>
2020-03-14 18:04:36 +0100

    Added MessageProducer, MessageConsumer and implementations

--- middleware/src/main/java/middleware/MessagingMiddleware.java
+++ middleware/src/main/java/middleware/MessagingMiddleware.java
@@ -1,10 +1,12 @@
 package middleware;
 
+import middleware.group.GroupManager;
+import middleware.group.GroupManagerImpl;
 import middleware.primitives.Primitive;
 
 import java.util.Map;
 
-public interface MessagingMiddleware <Key, Value, ApplicativePrimitive extends Enum<ApplicativePrimitive> & Primitive>{
+public interface MessagingMiddleware <Key, Value, ApplicativePrimitive extends Enum<ApplicativePrimitive> & Primitive> extends GroupManager {
     Map<Key, Value> join();
     void leave();
     void shareOperation(ApplicativePrimitive command, Key key, Value value);
