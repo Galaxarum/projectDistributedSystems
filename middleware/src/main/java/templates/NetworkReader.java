d99cc2a506a8b13c1211f444c3ce941756f32ee1
Galaxar <matteo.secco97@gmail.com>
2020-03-15 01:12:19 +0100

    Add NetworkReader, NetworkWriter Custom functional interfaces to read and write objects. Intended to be used with lambdas in order to allow specification of classes exploiting network communication transparently

---
+++ middleware/src/main/java/templates/NetworkReader.java
@@ -0,0 +1,6 @@
+package templates;
+
+@FunctionalInterface
+public interface NetworkReader {
+    Object readObject();
+}
---
+++ middleware/src/main/java/templates/NetworkWriter.java
@@ -0,0 +1,6 @@
+package templates;
+
+@FunctionalInterface
+public interface NetworkWriter {
+    void writeObject(Object object);
+}
