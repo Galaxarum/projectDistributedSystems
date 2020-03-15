f0378a7e973faa97773580cc2339674f52d7f271
Galaxar <matteo.secco97@gmail.com>
2020-03-15 01:40:21 +0100

    fixup! Update GroupManager Made actually an interface

--- middleware/src/main/java/middleware/MessagingMiddlewareImpl.java
+++ middleware/src/main/java/middleware/MessagingMiddlewareImpl.java
@@ -1,6 +1,7 @@
 package middleware;
 
 import middleware.group.GroupManager;
+import middleware.group.GroupManagerImpl;
 import middleware.primitives.Primitive;
 
 import java.util.Map;
@@ -13,7 +14,7 @@ public class MessagingMiddlewareImpl<Key,Value,ApplicationPrimitive extends Enum
 
 
     public MessagingMiddlewareImpl(String id, int port,String leaderHost) {
-        this.groupManager = new GroupManager<>(id, port,leaderHost);
+        this.groupManager = new GroupManagerImpl<>(id, port, leaderHost);
     }
 
     public MessagingMiddlewareImpl(String id,String leaderHost) {
