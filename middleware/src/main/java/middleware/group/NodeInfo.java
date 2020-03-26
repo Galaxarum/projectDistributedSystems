6e0bdb3618e8193bdfff5042426acd4bc4fddbaf
Galaxar <matteo.secco97@gmail.com>
2020-03-26 18:39:00 +0100

    Update NodeInfo -removed unneeded attributes -added NodeInfo.getSocket

--- middleware/src/main/java/middleware/group/NodeInfo.java
+++ middleware/src/main/java/middleware/group/NodeInfo.java
@@ -1,12 +1,22 @@
 package middleware.group;
 
+import exceptions.BrokenProtocolException;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.io.IOException;
+import java.net.Socket;
 
 @Getter @Setter
 public class NodeInfo{
 	private String hostname;
 	private int port;
-	private Integer timestamp;
-	private GroupCommands command;
+
+	public Socket getSocket(){
+		try {
+			return new Socket(hostname,port);
+		} catch (IOException e) {
+			throw new BrokenProtocolException("Impossible to establish connection with the replica "+hostname+":"+port);
+		}
+	}
 }
