25539eb421707b7cdf6ce1f2fdaeda0f3c8cb68d
Galaxar <matteo.secco97@gmail.com>
2020-04-13 20:05:34 +0200

    Update VectorClocks -Implemented VectorClocks.incrementLocal -Added a check for missing nodes in VectorClocks.compareTo

--- middleware/src/main/java/middleware/messages/VectorClocks.java
+++ middleware/src/main/java/middleware/messages/VectorClocks.java
@@ -2,7 +2,15 @@ package middleware.messages;
 
 import java.util.HashMap;
 
-public class VectorClocks extends HashMap<String, Integer> implements Comparable<VectorClocks>{
+public class VectorClocks extends HashMap<String, Integer> implements Comparable<VectorClocks> {
+
+    private final String localKey;
+
+    public VectorClocks(String localId) {
+        this.localKey = localId;
+        add(localId);
+    }
+
     public Integer add(String deviceId) {
         return super.put(deviceId, 0);
     }
@@ -12,34 +20,33 @@ public class VectorClocks extends HashMap<String, Integer> implements Comparable
     }
 
     public void update(VectorClocks vector) {
-        vector.forEach((k,v)->this.merge(k,v, Integer::max));
+        vector.forEach((k, v) -> this.merge(k, v, Integer::max));
     }
 
-    public void incrementLocal(VectorClocks vector) {
-        //TODO
+    public void incrementLocal() {
+        this.merge(localKey, 1, Integer::sum);
     }
 
     @Override
     public int compareTo(VectorClocks vectorClocks) {
-        /*assuming all vector clocks contain the key of all active processes (otherwise two vector clocks that don't
-        contain all the same processes should be considered concurrent (probably?) even if all other key-value pairs
-        indicate causality)
-         */
         int res = 0;
-        for (String k : vectorClocks.keySet()) {
-            if (res == 0) {
-                if (this.get(k) > vectorClocks.get(k))
-                    res = 1;
-                else if (this.get(k) < vectorClocks.get(k))
-                    res = -1;
+        for ( String k : vectorClocks.keySet() ) {
+            /*nodes existing in one and not in the other are ignored (by the following if, if the "extra" node is in vectorClocks,
+             *just not considered by the for loop if the extra node is in this
+             */
+            if ( this.containsKey(k) ) {
+                if ( res == 0 ) {
+                    if ( this.get(k) > vectorClocks.get(k) )
+                        res = 1;
+                    else if ( this.get(k) < vectorClocks.get(k) )
+                        res = -1;
+                } else if ( res == 1 ) {
+                    if ( this.get(k) < vectorClocks.get(k) )
+                        return 0;
+                } else    //res == -1
+                    if ( this.get(k) > vectorClocks.get(k) )
+                        return 0;
             }
-            else if (res == 1) {
-                if (this.get(k) < vectorClocks.get(k))
-                    return 0;
-            }
-            else    //res == -1
-                if (this.get(k) > vectorClocks.get(k))
-                    return 0;
         }
 
         return 0;   //equal or concurrent
