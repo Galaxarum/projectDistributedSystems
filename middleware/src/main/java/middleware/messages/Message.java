081c8cadfbed7f41dbacfba5d42bb0a5d2fd2232
Galaxar <matteo.secco97@gmail.com>
2020-07-27 16:59:54 +0200

    Update Message: added Message.compareTo (wraps VectorClock.compareTo)

--- middleware/src/main/java/middleware/messages/Message.java
+++ middleware/src/main/java/middleware/messages/Message.java
@@ -1,9 +1,15 @@
 package middleware.messages;
 
 import lombok.Data;
+import org.jetbrains.annotations.NotNull;
 
 @Data
-public class Message<T> {
+public class Message<T> implements Comparable<Message<T>>{
     private T content;
     private VectorClocks timestamp;
+
+    @Override
+    public int compareTo(@NotNull Message<T> tMessage) {
+        return timestamp.compareTo(tMessage.timestamp);
+    }
 }
